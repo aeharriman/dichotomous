ea389c1b93184cf3919b8fdb6c97f4bdc560f4e9
aeharriman <aeharrima@gmail.com>
2023-05-23 11:45:07 -0500

    Initial commit

---
+++ src/Components/Description/Description.tsx
@@ -0,0 +1,12 @@
+function Description() {
+
+    return <>
+    <div>
+        <h1>Dichotomous</h1>
+        <p>Turn a text dichotomous key into an interactive one</p>
+    </div>
+    </>
+
+}
+
+export default Description
---
+++ src/Components/InputPage/InputPage.tsx
@@ -0,0 +1,15 @@
+import Description from "../Description/Description";
+import InputForm from "../InputForm/InputForm";
+
+function InputPage () {
+
+
+return <>
+<Description />
+<InputForm />
+</>
+
+
+}
+
+export default InputPage
