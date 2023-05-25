d39e14b5c805712802d2783b0e74bd86019421d6
aeharriman <aeharrima@gmail.com>
2023-05-24 22:11:23 -0500

    added logic for history so you can go back

--- src/Components/ButtonTree/ButtonTree.tsx
+++ src/Components/ButtonTree/ButtonTree.tsx
@@ -5,19 +5,49 @@ import { error } from "console";
 
 function ButtonTree({dichotomousKey, setDichotomousKey}:{dichotomousKey: KeyObject|undefined, setDichotomousKey:React.Dispatch<React.SetStateAction<KeyObject|undefined>>}) {
 
+    const [currentIndex, setCurrentIndex] = useState<number>(1);
+    const [isDone, setIsDone] = useState<boolean>(false);
+    const [isStart, setIsStart] = useState<boolean>(true);
+    const [finalSelection, setFinalSelection] = useState<string>('');
+    const [history, setHistory] = useState<Array<number>>([]);
+
     // Sets Dichotomous key to undefined to trigger conditional rendering of parent component
     const handleGoBack = () => {
         setDichotomousKey(undefined);
     }
 
-    const [currentIndex, setCurrentIndex] = useState<number>(1);
-    const [isDone, setIsDone] = useState<boolean>(false);
+    const handlePrevious = () => {
+        if (history.length > 0) {
+        setIsDone(false);
+        setCurrentIndex(history[history.length-1]);
+        const previousHistory:Array<number> = history;
+        previousHistory.pop();
+        setHistory(previousHistory);
+        }
+    }
 
-    // TODO: come up with a better way to check dichotomousKey isnt defined before everything
+    useEffect (() => {
+        if (history.length != 0){
+            setIsStart(true)
+            console.log(history.length)
+        } else {
+            setIsStart(false)
+        }
+    },[history.length])
+
+    // TODO: come up with a better way to check dichotomousKey isn't defined before everything
     const handleSelection = (currentSubKey: string) => {
+        
         if (dichotomousKey !== undefined && typeof dichotomousKey[currentIndex][currentSubKey].goTo === "number") {
-            setCurrentIndex(Number(dichotomousKey[currentIndex][currentSubKey].goTo))
-        } else if (dichotomousKey !== undefined) {
+            const nextHistory:Array<number> = history;
+            nextHistory.push(currentIndex);
+            setHistory(nextHistory);
+            setCurrentIndex(Number(dichotomousKey[currentIndex][currentSubKey].goTo));
+        } else if (dichotomousKey !== undefined && typeof dichotomousKey[currentIndex][currentSubKey].goTo === "string") {
+            const nextHistory:Array<number> = history;
+            nextHistory.push(currentIndex);
+            setHistory(nextHistory);
+            setFinalSelection(String(dichotomousKey[currentIndex][currentSubKey].goTo));
             setIsDone(true);
         } else {
             throw new Error("Invalid dichotomous key (3)");
@@ -46,23 +76,22 @@ function ButtonTree({dichotomousKey, setDichotomousKey}:{dichotomousKey: KeyObje
     }
 
     return (dichotomousKey !== undefined) ? 
-        !isDone ?
         <> 
-        <p>{JSON.stringify(dichotomousKey)}</p>
+        {/* <p>{JSON.stringify(dichotomousKey)}</p> */}
 
-        <div style={{display: 'flex'}}>
-            {createButtons(dichotomousKey, currentIndex)}
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center',width: '500px', border: 'solid 5px black', padding: '3rem'}}>
+            {!isDone ? (
+                createButtons(dichotomousKey, currentIndex)
+            ) : (
+                <h1>{finalSelection}</h1>
+            )}
         </div>
 
-        <Button onClick={handleGoBack}>Go Back</Button>
-        </> : <>
-        <p>{JSON.stringify(dichotomousKey)}</p>
+        {isStart ? (
+        <Button onClick={handlePrevious}>Previous</Button>
+        ) : <></>}
 
-        <div style={{display: 'flex'}}>
-            {createButtons(dichotomousKey, currentIndex)}
-        </div>
-
-        <Button onClick={handleGoBack}>Go Back</Button>
+        <Button onClick={handleGoBack}>Go back to key entry page</Button>
         </> :
         <>
         Something went extremely wrong. You have fallen into the deep recesses of the program, 
