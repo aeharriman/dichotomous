51db4d45d433f7904c5f7049be314327f9e0f465
Austyn Harriman <aeharrima@gmail.com>
2023-08-23 11:37:54 -0500

    file versioning issue

---
+++ src/test/java/com/example/dichotomous/DichotomousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dichotomous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DichotomousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
